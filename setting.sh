c686cacd899e5216d0c3ef04573f506f11ea9912
katagiri <katagiri@eternal.(none)>
2013-05-09 23:51:36 +0900

    Added setting.sh

---
+++ setting.sh
@@ -0,0 +1,28 @@
+#/bin/sh
+
+DOTFILES_DIR=".dotfiles"
+
+# zshrc
+if [ -e ~/.zshrc ]; then
+  rm -f ~/.zshrc
+fi
+ln -s ~/$DOTFILES_DIR/.zshrc ~/.zshrc
+
+# vimrc
+if [ -e ~/.vimrc ]; then
+  rm -f ~/.vimrc
+fi
+ln -s ~/$DOTFILES_DIR/.vimrc ~/.vimrc
+
+# vim directory
+if [ -d ~/.vim ]; then
+  rm -rf ~/.vim
+fi
+ln -s ~/$DOTFILES_DIR/.vim ~/.vim
+git submodule update --init
+
+# tmux
+if [ -e ~/.tmux.conf ]; then
+  rm -f ~/.tmux.conf 
+fi
+ln -s ~/$DOTFILES_DIR/.tmux.conf ~/.tmux.conf
