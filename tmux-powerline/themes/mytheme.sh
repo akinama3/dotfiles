4223be2b6c9a2adefa94dbe428e57cd9f8808db9
Jun Katagiri <katagiri.jun2@gmail.com>
2013-06-29 22:05:09 +0900

    Modified: Weather Powerline

--- tmux-powerline/themes/mytheme.sh
+++ tmux-powerline/themes/mytheme.sh
@@ -46,8 +46,8 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 		#"cpu 37 255" \
 		#"load 237 167" \
 		#"tmux_mem_cpu_load 234 136" \
-		"battery 37 255" \
-		#"weather 37 255" \
+		#"battery 37 255" \
+		"weather 37 255" \
 		"rainbarf 0 0" \
 		#"xkb_layout 125 117" \
 		#"date_day 235 136" \
